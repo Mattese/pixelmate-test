935ac80aa105356312490a671313d437d78e4cca
martin.reichl@logsteo.com <martin.reichl@logsteo.com>
2021-02-23 17:10:20 +0100

    Added scss, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import 'src/styles/styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">testsdafasdf</header>
+      <header className="App-header">testsdafasd asdfasdf asdfaf a</header>
     </div>
   );
 }
