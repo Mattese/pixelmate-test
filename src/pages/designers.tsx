fd7209c70786c57a55c01f2f952ae5223e1506fd
martin.reichl@logsteo.com <martin.reichl@logsteo.com>
2021-02-28 20:46:11 +0100

    Skeleton loading added

--- src/pages/designers.tsx
+++ src/pages/designers.tsx
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { DesignerCard } from 'src/components/designerCard/designerCard';
+import { SkeletonDesignerCard } from 'src/components/designerCard/skeletonDesignerCard';
 import { SearchBar } from 'src/components/searchBar/searchBar';
 import { Designer } from 'src/types/types';
 import styles from './designers.module.scss';
 
 export const Designers: React.FC = () => {
   const [designers, setDesigners] = useState<Designer[]>([]);
+  const [loading, setLoading] = useState(false);
   const loadDesigners = async () => {
+    setLoading(true);
     const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
     setDesigners(data);
+    setLoading(false);
   };
   useEffect(() => {
     loadDesigners();
@@ -25,11 +29,19 @@ export const Designers: React.FC = () => {
       <div className={styles.cardsListSection}>
         <div className="row">
           <div className={styles.cardsWrapper}>
-            {designers.map((d, index) => {
-              if (index < 6) {
-                return <DesignerCard key={index} designer={d} />;
-              }
-            })}
+            {loading ? (
+              <>
+                <SkeletonDesignerCard />
+                <SkeletonDesignerCard />
+                <SkeletonDesignerCard />
+              </>
+            ) : (
+              designers.map((d, index) => {
+                if (index < 6) {
+                  return <DesignerCard key={index} designer={d} />;
+                }
+              })
+            )}
           </div>
         </div>
       </div>
